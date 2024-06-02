7b8e5ce2417256d5341aa6602cf6d39ea33115bd
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2024-06-03 00:54:03 +0600

    all trainer id api done

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ async function run() {
     res.send(result)
   })
 
+  app.get('/alltrainer/:id', async(req,res) => {
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)};
+    const result = await trainerCollection.findOne(query);
+    res.send(result)
+  })
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
